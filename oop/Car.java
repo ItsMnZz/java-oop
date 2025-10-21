d42c66e40e7e5c1452af21f02b91e1a2d5606887
ItsMnZz <mozthanakrid@gmail.com>
2025-10-21 13:44:40 +0700

    add car object

---
+++ oop/Car.java
@@ -0,0 +1,11 @@
+package oop;
+
+public class Car {
+
+    public String make = "Ford";
+    public String model = "Mustang";
+    public int year = 2025;
+    public double price = 10000.99;
+    public boolean isRunning = true;
+    
+}
---
+++ oop/Main.java
@@ -0,0 +1,18 @@
+package oop;
+
+public class Main {
+
+    public static void main(String[] args) {
+        System.out.println("Hello world");
+
+        Car car = new Car();
+
+        System.out.println(car.make);
+        System.out.println(car.model);
+        System.out.println(car.year);
+        System.out.println(car.price);
+        System.out.println(car.isRunning);
+
+    }
+
+}
